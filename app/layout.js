b8b6a20e4cfd050404fb7240bbf5fd5f59848a9f
Mel Schultz <100803766+mel-schultz@users.noreply.github.com>
2026-03-22 18:08:13 -0300

    restore v2

--- app/layout.js
+++ app/layout.js
@@ -57,24 +57,8 @@ export default function RootLayout({ children }) {
         </style>
       </head>
       <body>
-        <AppLayoutWrapper>
-          {children}
-        </AppLayoutWrapper>
+        {children}
       </body>
     </html>
-  )
-}
-
-// Componente separado para autenticação (Client Component)
-import { Suspense } from 'react'
-import AppLayoutClient from '../components/layout/AppLayoutClient'
-
-function AppLayoutWrapper({ children }) {
-  return (
-    <Suspense fallback={<div>Carregando...</div>}>
-      <AppLayoutClient>
-        {children}
-      </AppLayoutClient>
-    </Suspense>
   )
 }
